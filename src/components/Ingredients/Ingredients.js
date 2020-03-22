56ce14a09fddae3a48588524be1b6813e3922504
Gvin0 <levanigvino@gmail.com>
2020-03-23 00:02:31 +0400

    change useState with useReducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,47 +1,81 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal'
 import Search from './Search';
 
+
+// yvelaperi aq gvaq da reduceris gamoyeneba magito aris praqtikuli 
+
+//stateshi data avtomaturat modis by react tavidan [] shemdeg rac ingredients shi iqneba
+const ingredientReducer = (curState, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...curState, action.ingredient]
+    case 'DELETE':
+      return curState.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error('aq ra gindaa!!!');
+  }
+}
+
+const httpReducer = (httpState, action) => {
+  switch (action.type) {
+    case 'SEND_REQUEST_ADD':
+      return { ...httpState, addLoading: true, error: null }
+    case 'SEND_REQUEST_DELETE':
+      return { ...httpState, deleteLoading: true, error: null, deletedIngId: action.id }
+    case 'RESPONSE_ADD':
+      return { ...httpState, addLoading: false }
+    case 'RESPONSE_DELETE':
+      return { ...httpState, deleteLoading: false }
+    case 'ERROR_ADD':
+      return { ...httpState, addLoading: false, error: action.error }
+    case 'ERROR_DELETE':
+      return { ...httpState, deleteLoading: false, error: action.error }
+    case 'CLEAR':
+      return { ...httpState, error: null }
+    default:
+      throw new Error('aq ra gindaa!!!');
+  }
+}
+
 function Ingredients() {
-  const [ingredients, setIngredients] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isDeleteLoading, setIsDeleteLoading] = useState(false);
-  const [deletedId, setdeletedId] = useState('');
-  const [error, setError] = useState();
+  const [ingredientsState, dispatchIng] = useReducer(ingredientReducer, []); // tavidan [] wava state argumentshi 
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {
+    addLoading: false
+    , deleteLoading: false
+    , deletedIngId: null
+  });
 
   const addIngredientHandler = (ing) => {
-    setIsLoading(true);
+    dispatchHttp({ type: 'SEND_REQUEST_ADD' });
     fetch('https://react-hooks-add7b.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ing),
       headers: { 'Content-Type': 'application.json' }
     }).then(response => {
-      setIsLoading(false);
+      dispatchHttp({ type: 'RESPONSE_ADD' });
       return response.json();
     }).then(responseData => {
-      setIngredients(prevIngredients => [
-        ...prevIngredients,
-        { id: responseData.name, ...ing }
-      ]);
-    })
-
+      dispatchIng({ type: 'ADD', ingredient: { id: responseData.name, ...ing } });
+    }).catch(error => {
+      dispatchHttp({ type: 'ERROR_ADD', error: error.message });
+    });
   };
 
   const removeIngredientHandler = (id) => {
-    setIsDeleteLoading(true);
-    setdeletedId(id);
+    dispatchHttp({ type: 'SEND_REQUEST_DELETE', id: id });
     fetch(`https://react-hooks-add7b.firebaseio.com/ingredients/${id}.json`, {
       method: 'DELETE'
     }).then(response => {
-      setIsDeleteLoading(false);
-      setIngredients(prevIngredients => prevIngredients.filter(ing => ing.id !== id));
+      dispatchHttp({ type: 'RESPONSE_DELETE' });
+      dispatchIng({ type: 'DELETE', id: id });
     }).catch(error => {
-      //ony one render cycle - react batches setStates
-      setError(error.message);
-      setIsDeleteLoading(false); 
+      dispatchHttp({ type: 'ERROR_DELETE', error: error.message });
     });
   };
 
@@ -53,26 +87,26 @@ function Ingredients() {
   // axal variants ar gadacems shvils - 
   // gadacems igive anu dzvel variants da igive ukve agar iwvevs axlidan shvilis renders
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-    setIngredients(filteredIngredients);
+    dispatchIng({ type: 'SET', ingredients: filteredIngredients });
   }, []);
 
-  const closeHandler = () => {  
-    setError(null);
+  const closeHandler = () => {
+    dispatchHttp({ type: 'CLEAR' });
   }
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={closeHandler}> {error} </ErrorModal>}
-      <IngredientForm onAddIngredient={addIngredientHandler} 
-        loading={isLoading}
+      {httpState.error && <ErrorModal onClose={closeHandler}> {httpState.error} </ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler}
+        loading={httpState.addLoading}
       />
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
-        <IngredientList 
-          ingredients={ingredients} 
-          onRemoveItem={removeIngredientHandler} 
-          deleteLoading={isDeleteLoading}
-          deletedItem={deletedId}/>
+        <IngredientList
+          ingredients={ingredientsState}
+          onRemoveItem={removeIngredientHandler}
+          deleteLoading={httpState.deleteLoading}
+          deletedItem={httpState.deletedIngId} />
       </section>
     </div>
   );
